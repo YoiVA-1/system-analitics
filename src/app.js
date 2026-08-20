dac898d6ee59d5225c3fba75f02247a96addd884
YoiVA-1 <yoinervelez8@gmail.com>
2026-08-19 23:16:43 -0500

    actualizacion completa del system

--- src/app.js
+++ src/app.js
@@ -1,33 +1,55 @@
 import chalk from 'chalk';
+import os from 'node:os';
 
 console.log(chalk.green.bold("=== Inicio del sistema ===\n"));
 
-console.time(chalk.yellow('⏱️ ProcesoPrincipal'));
-
-function accesoUsuario(usuario) {
-console.count(chalk.blue(`👤 Acceso de usuario ${usuario}`));
-}
-
-// Simulando accesos
-accesoUsuario('Carlos');
-accesoUsuario('Ana');
-accesoUsuario('Carlos');
-
-// Advertencia
-console.warn(chalk.redBright('⚠️  Capacidad de usuarios alcanzando el límite'));
-
-// Error
-console.error(chalk.red.bold('❌ Error: No se pudo conectar a la base de datos'));
-
-// Mostrar usuarios como tabla
 const usuarios = [
-  { nombre: "Carlos", rol: "Admin" },
-  { nombre: "Ana", rol: "User" }
+  { nombre: "Rodrigo", rol: "Admin" },
+  { nombre: "Juan", rol: "User" },
+  { nombre: "roberto", rol: "Mod" }
 ];
 
-console.log(chalk.cyan('\n📋 Lista de usuarios conectados:'));
-console.table(usuarios);
+console.log(chalk.green("____________Bienvenido___________"))
 
-console.timeEnd(chalk.yellow('⏱️ ProcesoPrincipal'));
+console.log(chalk.yellow("Opciones: datos, hola, sistema, fin"))
 
-console.log(chalk.green.bold("\n=== Fin del sistema ==="));
+process.stdin.setEncoding('utf-8');
+process.stdin.on('data', (data) => {
+  const input = data.trim().toLowerCase();
+
+  switch (input) {
+    case 'hola':
+      console.log(chalk.green.bold('¡Hola! ¿Cómo estás?'));
+      break;
+    case 'datos':
+      console.log(chalk.cyan('\n📋 Lista de usuarios conectados:'));
+      console.table(usuarios)
+      break;
+    case 'sistema':
+      function mostrarInformacion() {
+        console.clear();
+
+        console.log(chalk.cyan('🖥️  Monitor de Sistema'));
+        console.log(chalk.red.bold('========================'));
+        console.log(chalk.yellowBright(`Sistema: ${os.platform()} (${os.arch()})`));
+        console.log(chalk.yellowBright(`CPU: ${os.cpus()[0].model}`));
+        console.log(chalk.yellowBright(`Cores: ${os.cpus().length}`));
+        console.log(chalk.yellowBright(`Memoria Libre: ${(os.freemem() / (1024 * 1024)).toFixed(2)} MB`));
+        console.log(chalk.yellowBright(`Memoria Total: ${(os.totalmem() / (1024 * 1024)).toFixed(2)} MB`));
+        console.log(chalk.yellowBright(`Uptime: ${(os.uptime() / 60).toFixed(2)} minutos`));
+        console.log(chalk.yellowBright(`Usuario: ${os.userInfo().username}`));
+        console.log(chalk.red.bold('========================\n'));
+      }
+
+      mostrarInformacion();
+      break
+    case 'fin':
+      console.log(chalk.cyan('finalizando...'));
+      process.exit(0);
+      break;
+    default:
+      console.log(chalk.red('❌ Comando no reconocido ❌'));
+  }
+
+  process.stdout.write(chalk.green('Ingresa un nuevo comando: '));
+});
